23d06d4b76751c2c215450a99b4c45876dbc1d9c
Ricky Donato <rickj07@gmail.com>
2015-12-18 21:10:53 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="common-tools",
+    version="1.0",
+    author="Rick Donato",
+    author_email="rickj07@gmail.com",
+    description="common-tools"
+)
