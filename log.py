7bc68e8eff13dbe7104a37a5d0a8c715e600ec2d
Ricky Donato <rickj07@gmail.com>
2016-10-09 22:07:51 +0100

    Update log.py

--- log.py
+++ log.py
@@ -5,18 +5,26 @@ from functools import wraps
 def class_method_logger(func):
     @wraps(func)
     def wrapper(self, *args, **kwargs):
-        logging.debug("input args=%s kwargs=%s" % (str(args),str(kwargs)))
-        response = func(self, *args, **kwargs)
-        logging.debug("return=%s", str(response))
-        return response
+        try:    
+            logging.debug("input args=%s kwargs=%s" % (str(args),str(kwargs)))
+            response = func(self, *args, **kwargs)
+            logging.debug("return=%s", str(response))
+            return response
+        except Exception as exc:
+            logging.exception(exc)
+            raise     
     return wrapper
 
 def method_logger(func):
     def wrapper(*args, **kwargs):
-        logging.debug("input args=%s kwargs=%s" % (str(args),str(kwargs)))
-        response = func(*args, **kwargs)
-        logging.debug("return=%s", str(response))
-        return response
+        try:
+            logging.debug("input args=%s kwargs=%s" % (str(args),str(kwargs)))
+            response = func(*args, **kwargs)
+            logging.debug("return=%s", str(response))
+            return response
+        except Exception as exc:
+            logging.exception(exc)
+            raise
     return wrapper
 
 class_logger = decorate_all_methods(class_method_logger)
