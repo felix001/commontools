b629f1aefb230caa5273d8ffe70eec9afd2ea3e4
Rick Donato <rickj07@gmail.com>
2017-10-24 07:24:50 +0100

    Update file.py

--- file.py
+++ file.py
@@ -1,26 +1,10 @@
-import json
-import os
+def read_file(filename):
+    file = open(filename, "r")
+    data = file.read()
+    file.close()
+    return data
 
-def write_file(data=None, path=False, filename=None, json_dump=False):
-    """
-    Write data to file
-
-    Args:
-        data: data to write to file
-        path: path to file. if False path=home_dir
-        filename: filename
-        json_dump: 
-
-    Returns: Bool()
-    """
-    if not path:
-        path = os.environ['HOME'] + '/'
-    filepath = "%s/%s" % (path, filename)
-    f = open(filepath, 'w')
-    os.chmod(filepath, 0600)
-    if json_dump:
-        f.write("%s" % json.dumps(data))
-    else:
-        f.write("%s" % data)
-    f.close
-    return True
+def write_file(data, filename):
+    file = open(filename, "w")
+    file.write(data)
+    file.close()
