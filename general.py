06d35557edb8113107734b16bf1094fd377246ca
Rick Donato <rickj07@gmail.com>
2017-11-15 15:29:34 +0000

    Delete general.py

--- general.py
+++
@@ -1,8 +0,0 @@
-import inspect
-
-def decorate_all_methods(decorator):
-    def decorate(cls):
-        for name, fn in inspect.getmembers(cls, inspect.ismethod):
-            setattr(cls, name, decorator(fn))
-        return cls
-    return decorate
