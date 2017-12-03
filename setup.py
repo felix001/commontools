e68a1cc4d2894abc6de254278a985c757399e085
Rick Donato <rickj07@gmail.com>
2017-12-03 21:42:04 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="commontools",
     version="2.0.1",
     author="Rick Donato",
-    author_email="rickj07@gmail.com",
+    author_email="rick@donato.me.uk",
     description="commontools",
     py_modules = ['convert.convert', 'log.log', 'general.general', 'file.file']
 )
