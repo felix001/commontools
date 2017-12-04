23bcfc3f57d1cd4bd96113bf01156f4af87cc3db
Rick Donato <rickj07@gmail.com>
2017-12-04 21:30:04 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="commontools",
-    version="2.0.1",
+    version="2.0.2",
     author="Rick Donato",
     author_email="rick@donato.me.uk",
     description="commontools",
-    py_modules = ['convert.convert', 'log.log', 'general.general', 'file.file']
+    packages=['convert', 'log', 'general', 'file']
 )
