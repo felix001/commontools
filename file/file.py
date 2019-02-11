28d6e205b945eeb48ec5d39aa368625b1f7a6e61
PacketPimp3 <rickj07@gmail.com>
2019-02-11 16:31:34 +0000

    Update file.py

--- file/file.py
+++ file/file.py
@@ -1,3 +1,9 @@
+import yaml
+
+def read_yaml_file(self, full_path):
+    with open(full_path) as yamlfile:
+        return yaml.load(yamlfile)
+    
 def read_file(filename):
     file = open(filename, "r")
     data = file.read()
